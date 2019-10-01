94965b6b9a4a72cf6c806efb4e7588685703ca51
AshishTaduce <ashishteddy2@gmail.com>
2019-10-01 10:54:11 +0530

    Indian Flag

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  Container container1 = Container(
+    color: Colors.deepOrangeAccent,
+    height: 100,
+  );
+
+  Container container2 = Container(
+    color: Colors.white,
+    height: 100,
+    //width: double.infinity,
+    child: Center(
+
+      child: Image(
+        image: NetworkImage('https://raw.githubusercontent.com/McLarenCollege/Flutter-Course-Notes/master/ensign.png'),
+
+      ),
+    ),
+  );
+
+  Container container3 = Container(
+    color: Colors.lightGreen,
+    height: 100,
+  );
+
+
+
+
+
+  Text text = Text('Hello world');
   runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.amberAccent,
-        body: Container(),
+        backgroundColor: Colors.black38,
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.fromLTRB(0, 100, 0, 50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[container1, container2, container3],
+
+          ),
+        )),
       ),
     ),
   );
